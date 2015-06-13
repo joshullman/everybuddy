10e80fccf5096c714cf39a862bf3d7eb4c768094
joshullman <joshullman@gmail.com>
2015-06-13 15:35:23 -0700

    Fix seed so that everyone has different email

--- app/controllers/results.rb
+++ app/controllers/results.rb
@@ -3,12 +3,18 @@ get '/results' do
 end
 
 post '/results' do
+  p "I'm getting here!!!"
+  p params.inspect
   @users = []
+  a = User.all
+  p a
 
   User.all.each do |user|
    user.tags.each do |tag|
      @users << user if tag.name == params[:tag]
    end
   end
+
+  p @users
   erb :results
 end
