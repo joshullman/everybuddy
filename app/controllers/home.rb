e0160600874011720537103cda9f9a9a83beec09
joshullman <joshullman@gmail.com>
2015-06-13 15:05:48 -0700

    Add working search

--- app/controllers/home.rb
+++ app/controllers/home.rb
@@ -2,6 +2,9 @@ get '/' do
   erb :home
 end
 
+post '/' do
+end
+
 # get '/search' do
 # 	redirect '/login' unless logged in?
 # end
--- app/controllers/results.rb
+++ app/controllers/results.rb
@@ -3,5 +3,12 @@ get '/results' do
 end
 
 post '/results' do
+  @users = []
 
+  User.all.each do |user|
+   user.tags.each do |tag|
+     @users << user if tag.name == params[:tag]
+   end
+  end
+  erb :results
 end
