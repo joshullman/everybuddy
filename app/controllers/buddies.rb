d4028cda66b83f95a98bbdb6c1ecc34defe3802e
Josh Ullman <joshullman@gmail.com>
2015-06-20 16:00:45 -0700

    Tighten up buddy routes to eliminate redundant user_id routes

--- app/controllers/buddies.rb
+++ app/controllers/buddies.rb
@@ -1,31 +1,31 @@
 #all accepted
-get	"/users/:user_id/buddies" do
-  @user = User.find(params[:user_id])
+get	"/buddies" do
+  @user = User.find(current_user.id)
   @buddies = @user.accepted_buddies
 	erb :"buddies/index"
 end
 
 #pending
-get "/users/:user_id/buddies/pending" do
-  @user = User.find(params[:user_id])
+get "/buddies/pending" do
+  @user = User.find(current_user.id)
   @buddies = @user.pending_buddies
   erb :"buddies/pending"
 end
 
 #requests
-get "/users/:user_id/buddies/requests" do
-  @user = User.find(params[:user_id])
+get "/buddies/requests" do
+  @user = User.find(current_user.id)
   @buddies = @user.buddy_requests
   erb :"buddies/requests"
 end
 
 # create/accept new friend
-post "/users/:user_id/buddies/:buddy_id/new" do
+post "/buddies/:buddy_id/new" do
   @buddy_rel = BuddyRelationship.where(user_id: params[:buddy_id], buddy_id: current_user.id).first || 
                BuddyRelationship.where(user_id: current_user.id, buddy_id: params[:buddy_id]).first
   @buddy_rel[:accepted] = true
   if @buddy_rel.save
-    redirect "/users/#{current_user.id}/buddies/requests"
+    redirect "/buddies/requests"
   else
     "I GOT AN ERROR D:"
   end
@@ -38,18 +38,18 @@ post "/users/:user_id/buddies" do
 end
 
 # #destroy
-delete	"/users/:user_id/buddies/:buddy_id/delete" do
-  @buddy = BuddyRelationship.where(user_id: params[:buddy_id], buddy_id: current_user.id).first || 
+delete	"/buddies/:buddy_id/delete" do
+  @buddy_rel = BuddyRelationship.where(user_id: params[:buddy_id], buddy_id: current_user.id).first || 
            BuddyRelationship.where(user_id: current_user.id, buddy_id: params[:buddy_id]).first
-  @buddy.destroy
+  @buddy_rel.destroy
   p "Removed Buddy"
-  redirect "/users/#{params[:user_id]}/buddies"
+  redirect "/buddies"
 end
 
-delete  "/users/:user_id/buddies/:buddy_id" do
-  @buddy = BuddyRelationship.where(user_id: params[:buddy_id], buddy_id: current_user.id).first || 
+delete  "/buddies/:buddy_id" do
+  @buddy_rel = BuddyRelationship.where(user_id: params[:buddy_id], buddy_id: current_user.id).first || 
            BuddyRelationship.where(user_id: current_user.id, buddy_id: params[:buddy_id]).first
-  @buddy.destroy
+  @buddy_rel.destroy
   p "Removed Buddy"
-  redirect "/users/#{params[:buddy_id]}"
+  redirect "/buddies/requests"
 end
