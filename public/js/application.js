3c32b91e064a62788405de3192cc634e1871c3c6
Josh Ullman <joshullman@gmail.com>
2015-06-21 14:13:30 -0700

    add star thing that I don't know how to use

--- public/js/application.js
+++ public/js/application.js
@@ -4,4 +4,29 @@ $(document).ready(function() {
   // when we try to bind to them
 
   // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+
+  starFun();
+
 });
+
+var starFun = function() {  
+
+		$(".star-off").on("click", function(event){
+
+    	// var url = $(this).attr("href");
+
+    event.preventDefault();
+
+     $.ajax({
+        type: "get",
+        url:url,
+        dataType: "json"
+      })
+
+      .done(function(response){
+        $(".star-off").removeClass();
+        $(".star-on").addClass();
+
+      })
+    }
+  };
